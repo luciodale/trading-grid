9f115b36ffac242a5e5f719024b853f36fc22b25
Lucio D'Alessandro <lda@juxt.pro>
2025-08-25 00:51:42 +0200

    add link to site

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,13 +2,13 @@ export function Header() {
 	return (
 		<header className="app-header">
 			<div className="header-content">
-				<div className="logo-container">
+				<a href="https://koolcodez.com" className="logo-container">
 					<img
 						src="/kool-codez-inline-logo.svg"
 						alt="Kool Codez"
 						className="logo"
 					/>
-				</div>
+				</a>
 				<div className="header-title">
 					<h1>Options Trading Dashboard</h1>
 					<p>Real-time streaming data with AG Grid</p>
